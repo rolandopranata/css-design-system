8be01b1482931a77f8ca67a21fa6acdfe48ad16d
RolandoPranataAR <rolandopranata@gmail.com>
2022-05-25 23:50:39 +0700

    add: finishing tabs function

--- tabs.js
+++ tabs.js
@@ -15,19 +15,17 @@ function changeTabFocus(e) {
 
     if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
         tabs[tabFocus].setAttribute('tabindex', -1);
-    }
 
-    if (e.keyCode === keydownRight) {
-        tabFocus++;
-        if (tabFocus >= tabs.length) {
-            tabFocus = 0;
-        }
-    }
-
-    if (e.keyCode === keydownLeft) {
-        tabFocus--;
-        if (tabFocus < 0) {
-            tabFocus = tabs.length - 1;
+        if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length) {
+                tabFocus = 0;
+            }
+        } else if (e.keyCode === keydownLeft) {
+            tabFocus--;
+            if (tabFocus < 0) {
+                tabFocus = tabs.length - 1;
+            }
         }
     }
 
@@ -38,12 +36,30 @@ function changeTabFocus(e) {
 function changeTabPanel(e) {
     const targetTab = e.target;
     const targetPanel = targetTab.getAttribute('aria-controls');
+    const targetImage = targetTab.getAttribute('data-image');
+
     const tabContainer = targetTab.parentNode;
     const mainContainer = tabContainer.parentNode;
 
-    mainContainer
-        .querySelectorAll('article')
-        .forEach((article) => article.setAttribute('hidden', true));
+    tabContainer
+        .querySelector('[aria-selected="true"]')
+        .setAttribute('aria-selected', false);
 
-    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+    targetTab.setAttribute('aria-selected', true);
+
+    hideContent(mainContainer, `[role="tabpanel"]`);
+    showContent(mainContainer, `#${targetPanel}`);
+
+    hideContent(mainContainer, 'picture');
+    showContent(mainContainer, `#${targetImage}`);
+}
+
+function hideContent(parent, content) {
+    parent
+        .querySelectorAll(content)
+        .forEach((item) => item.setAttribute('hidden', true));
+}
+
+function showContent(parent, content) {
+    parent.querySelector(content).removeAttribute('hidden');
 }
